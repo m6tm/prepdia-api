c0114bf5e4c21fdf1107b0b633127eefd5533acf
maboa daniel emmanuel <maboadaniel.55@gmail.com>
2023-01-17 09:03:36 +0100

    Nouvelle état sur Gît

---
+++ database/migrations/2023_01_17_075446_create_tutors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('CASCADE');
+            $table->string('title');
+            $table->float('tarif')->default(0);
+            $table->longText('about');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutors');
+    }
+};
