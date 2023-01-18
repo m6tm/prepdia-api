bee530cee848d7f5e795127ebf0ea4be64bea177
maboa daniel emmanuel <maboadaniel.55@gmail.com>
2023-01-18 14:40:50 +0100

    Ajout des champs dans la table answers

--- database/migrations/2023_01_18_131036_create_answers_table.php
+++ database/migrations/2023_01_18_131036_create_answers_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('question_id')->constrained()->onDelete('CASCADE');
+            $table->string('answer');
             $table->softDeletes();
             $table->dateTime('updated_at')->useCurrent();
             $table->dateTime('created_at')->useCurrent();
